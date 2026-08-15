fc10d8ce5ac9ec3a95cb80b3b6ae69291819e9f2
fahadsoomro123 <166254374+fahadsoomro123@users.noreply.github.com>
2026-08-15 14:59:01 +0500

    Add Google Mobile Ads SDK

--- NexusNovaAndroid/app/build.gradle.kts
+++ NexusNovaAndroid/app/build.gradle.kts
@@ -43,4 +43,5 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.webkit:webkit:1.10.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("com.google.android.gms:play-services-ads:25.4.0")
 }
