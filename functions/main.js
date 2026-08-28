c70b2e1728bcd4244bae3306d43f46be0dadfa53
Fahad Hussain <166254374+fahadsoomro123@users.noreply.github.com>
2026-08-28 18:40:58 +0500

    Wire NOVA atomic chain backend functions

--- functions/main.js
+++ functions/main.js
@@ -6,3 +6,4 @@ Object.assign(exports, require("./learningSearch"));
 Object.assign(exports, require("./entertainmentSearch"));
 Object.assign(exports, require("./providerHealth"));
 Object.assign(exports, require("./novaBrainRegistry"));
+Object.assign(exports, require("./novaAtomicChain"));
