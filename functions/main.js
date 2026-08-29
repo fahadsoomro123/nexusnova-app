e31d5ba8e9e5bead476925c2154c194d929e4529
Fahad Hussain <166254374+fahadsoomro123@users.noreply.github.com>
2026-08-29 19:59:06 +0500

    cleanup: make apk-builds artifact-only while preserving latest Gemini reset APK

--- functions/main.js
+++
@@ -1,9 +0,0 @@
-Object.assign(exports, require("./index"));
-Object.assign(exports, require("./travelSearch"));
-Object.assign(exports, require("./hotelSearch"));
-Object.assign(exports, require("./groundTransportSearch"));
-Object.assign(exports, require("./learningSearch"));
-Object.assign(exports, require("./entertainmentSearch"));
-Object.assign(exports, require("./providerHealth"));
-Object.assign(exports, require("./novaBrainRegistry"));
-Object.assign(exports, require("./novaAtomicChain"));
