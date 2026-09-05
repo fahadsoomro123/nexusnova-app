616633d4efce0093d1fb1e05e5cc0bbacc597afb
Fahad Hussain <166254374+fahadsoomro123@users.noreply.github.com>
2026-09-05 13:56:18 +0500

    feat: export Pakistan agency travel functions

--- functions/main.js
+++ functions/main.js
@@ -2,6 +2,7 @@ Object.assign(exports, require("./index"));
 Object.assign(exports, require("./travelSearch"));
 Object.assign(exports, require("./hotelSearch"));
 Object.assign(exports, require("./groundTransportSearch"));
+Object.assign(exports, require("./pakistanAgencySearch"));
 Object.assign(exports, require("./learningSearch"));
 Object.assign(exports, require("./entertainmentSearch"));
 Object.assign(exports, require("./providerHealth"));
